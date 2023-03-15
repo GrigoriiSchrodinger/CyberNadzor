c7dd3feedf9b1dc472192b90fe84a2cfc2e0306d
Grigorii Schtodinger <schrodinger@MacBook-Pro-Grigorii.local>
2023-03-16 01:05:10 +0300

    Added user verification in the database and various dialogs

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from aiogram import types
 from aiogram.utils import executor
 import settings
+from asset.dialogues import start_dialog_not_registered, start_dialog_is_registered
+from asset.text_logo import logo
 from loader import dp, db
 
 logger = settings.setup_custom_logger('root')
@@ -8,12 +10,16 @@ logger = settings.setup_custom_logger('root')
 
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    db.add_users(
-        username=message.chat.username,
-        last_name=message.chat.last_name,
-        first_name=message.chat.first_name,
-        id_user=message.chat.id
-    )
+    if db.check_user(message.chat.id) is None:
+        db.add_user(
+            username=message.chat.username,
+            last_name=message.chat.last_name,
+            first_name=message.chat.first_name,
+            id_user=message.chat.id
+        )
+        await message.answer(start_dialog_not_registered.format(name=message.chat.first_name))
+    else:
+        await message.answer(start_dialog_is_registered.format(name=message.chat.first_name))
 
 
 async def on_startup(dp):
@@ -21,4 +27,5 @@ async def on_startup(dp):
 
 
 if __name__ == "__main__":
+    logger.info(logo)
     executor.start_polling(dp, on_startup=on_startup, skip_updates=False)
