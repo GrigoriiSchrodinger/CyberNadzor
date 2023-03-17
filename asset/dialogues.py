641c5bd5d795f123416ee95145800a356a9d4522
“Schrodinger” <ilovan@cyberborea.tech>
2023-03-17 11:40:06 +0300

    ref dialogues.py

--- asset/dialogues.py
+++ asset/dialogues.py
@@ -1,3 +1,5 @@
-start_dialog_not_registered: str = "Привет {name}, меня зовут CyberNadzor"
+start_dialog_not_registered: str = "Привет {name}, меня зовут CyberNadzor, я могу вывести актуальный курс bitcoin." \
+                                   "Также сообщать, когда курс поднимется выше нужного значения или наоборот опуститься\n\n" \
+                                   "/bitcoin - чтобы вывести курс bitcoin"
 
 start_dialog_is_registered: str = " Чем могу помочь? {name}, может вывести команду /help?"
