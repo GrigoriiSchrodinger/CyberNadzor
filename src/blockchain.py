47d8b0197aa814e39dbdcdedb2bcc92a8041fc1a
Grigorii Schtodinger <schrodinger@MacBook-Pro-Grigorii.local>
2023-03-18 20:04:41 +0300

    add types

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,5 +1,6 @@
 import logging
 import requests
+from requests import Response
 
 logger = logging.getLogger('root')
 
@@ -8,7 +9,7 @@ class BitcoinAPI:
     def __init__(self):
         self.url: str = "https://api.blockchain.com/v3/exchange"
 
-    def get(self, end_point):
+    def get(self, end_point) -> Response:
         logger.info(f"Request - {self.url + end_point}")
         response = requests.get(url=self.url + end_point)
         logger.info(f"Response {response.status_code} - {response.json()}")
@@ -20,20 +21,20 @@ class BlockChain(BitcoinAPI):
     #     for x in range(20):
     #         print(x)
     #         await asyncio.sleep(1)
-    def get_currency(self, end_point):
+    def get_currency(self, end_point: str) -> str:
         return "{:,}".format(self.get(end_point).json()["last_trade_price"])
 
-    def price_bitcoin(self):
-        return self.get_currency("/tickers/BTC-USD")
+    def price_bitcoin(self) -> str:
+        return self.get_currency(end_point="/tickers/BTC-USD")
 
-    def price_ethereum(self):
-        return self.get_currency("/tickers/ETH-USD")
+    def price_ethereum(self) -> str:
+        return self.get_currency(end_point="/tickers/ETH-USD")
 
-    def price_litecoin(self):
-        return self.get_currency("/tickers/LTC-USD")
+    def price_litecoin(self) -> str:
+        return self.get_currency(end_point="/tickers/LTC-USD")
 
-    def price_dogecoin(self):
-        return self.get_currency("/tickers/DOGE-USD")
+    def price_dogecoin(self) -> str:
+        return self.get_currency(end_point="/tickers/DOGE-USD")
 
-    def price_cardano(self):
-        return self.get_currency("/tickers/ADA-USD")
+    def price_cardano(self) -> str:
+        return self.get_currency(end_point="/tickers/ADA-USD")
